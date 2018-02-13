5482b450885f5815c635c7a3adaa533c48bb2abe
Øystein Sigholt <oysteinsigholt@users.noreply.github.com>
2018-02-13 00:06:53 -0800

    add folder existence check

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const fs = require('fs');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const nodemon = require('gulp-nodemon');
 const livereload = require('gulp-livereload');
+
+gulp.task('setup', (callback) => {
+  const userDir = path.resolve(__dirname, 'data', 'users');
+
+  fs.stat(userDir, (err) => {
+    if (err && err.errno === 34) {
+      fs.mkdir(userDir, callback);
+    } else {
+      callback(err);
+    }
+  });
+});
 
 gulp.task('sass', () => {
   gulp.src('./sass/main.scss')
@@ -29,4 +42,4 @@ gulp.task('watch', () => {
 });
 
 gulp.task('dev', ['sass', 'watch']);
-gulp.task('build', ['sass']);
+gulp.task('build', ['sass', 'setup']);
