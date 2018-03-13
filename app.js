609b432ec7cb63e7061e1f15394f53b5d7ad99db
Øystein Sigholt <oysteinsigholt@users.noreply.github.com>
2018-03-12 17:43:50 -0700

    it's week 10 mah dudes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const env = process.env.NODE_ENV || 'dev';
 let callbackURL = 'http://localhost:8080/auth/google/callback';
 
 if (env !== 'dev') {
-  callbackURL = 'https://a9-ucsdplan.herokuapp.com/auth/google/callback';
+  callbackURL = 'https://a10-ucsdplan.herokuapp.com/auth/google/callback';
 }
 
 function sumTxt(txt) {
